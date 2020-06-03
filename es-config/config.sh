5265ecca978d8df7d9bc881f743faf6f38822c85
kledenai <bruno.silva1643@etec.sp.gov.br>
2020-06-03 13:04:32 -0300

    feat: update es-config

--- es-config/config.sh
+++ es-config/config.sh
@@ -2,8 +2,6 @@
 
 sudo docker config create es-coordination es-coordination.yml
 
-sudo docker config create es-master es-master.yml
-
 sudo docker config create es-master1 es-master1.yml
 
 sudo docker config create es-master2 es-master2.yml
